ff2457f48e2b92776a47b35899abf6d2de5a4073
Dipak Anandrao Vaidya <97924550+DipakAVaidya@users.noreply.github.com>
2024-05-26 16:01:25 +0530

    Add files via upload

---
+++ studentDataBaseApp/StudentDataBaseApp.java
@@ -0,0 +1,15 @@
+package studentDataBaseApp;
+
+public class StudentDataBaseApp {
+
+	public static void main(String[] args) {
+		
+		Student stu1 = new Student();
+		stu1.enroll();
+		
+		// ask how many user we want to add
+		
+		//Create n numberr 
+	}
+
+}
